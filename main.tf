db957fb8e778b800f8d01477fac25762d466cb4a
Didier Gourillon <dgourillon@google.com>
2022-10-07 16:13:24 +0200

    add a win instance and replace the test linux by tomcat

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 
-resource "google_compute_instance" "default" {
-  name         = "test"
+resource "google_compute_instance" "tomcat1" {
+  name         = "tomcat-1"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
@@ -10,6 +10,42 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+
+    # access_config {
+    #   // Ephemeral public IP
+    # }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+
+
+resource "google_compute_instance" "bastion" {
+  name         = "bastion"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/windows-cloud/global/images/windows-server-2019-dc-v20220915"
     }
   }
 
